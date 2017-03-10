640efb8bc038a3e4ecbd6c97f1ecbbfaf8770b13
Patrick Bush <patrick@wahoofitness.com>
2017-03-09 21:00:07 -0600

    Client: Created tests; Client: Process request, process error detection, process bad arguments, process dead server, add method comments, refactor by making client architecture more like server architecture; Server: cleanup imports, modify method publicity, send HTTP response, send new packet headers, add error correction checksum

--- Project 1/NetworkClient/src/UDPClient.java
+++ Project 1/NetworkClient/src/UDPClient.java
@@ -1,21 +1,30 @@
 /**
  * Created by King on 3/2/2017.
  */
-import javax.xml.crypto.Data;
+
 import java.io.*;
 import java.net.*;
 import java.nio.file.Files;
+import java.util.Arrays;
 
 import static java.lang.System.exit;
 
-
+/**
+ * Main UDPClient
+ */
 class UDPClient {
-    public static final int PACKET_LENGTH = 128;
-    public static DatagramSocket clientSocket;
-    public static int damagedPacketProbability;
-    public static String fileName;
+    static final int PACKET_LENGTH = 128;
+    static DatagramSocket clientSocket;
+    static int damagedPacketProbability;
+    static String fileName;
+    static UDPClientHelper client;
 
-
+    /**
+     * Main UDPClient method
+     *
+     * @param args can include the corruptibility percentage
+     * @throws Exception if unable to handle some exception
+     */
     public static void main(String args[]) throws Exception {
         // check if a damaged packet probability was passed in
         if (args.length > 0) {
@@ -29,38 +38,67 @@ class UDPClient {
             }
         }
 
-        clientSocket = new DatagramSocket();
-        UDPClientHelper client = new UDPClientHelper("127.0.0.1");
+        // begin client runloop
+        kickoffRunloop();
+    }
 
-        while(true) {
+    /**
+     * Main runloop of the client
+     */
+    private static void kickoffRunloop() {
+        // always be prepared to accept a HTTP request input
+        while (true) {
             BufferedReader inputBuffer = new BufferedReader(new InputStreamReader(System.in));
             try {
+                clientSocket = new DatagramSocket();
+                client = new UDPClientHelper("127.0.0.1");
+                System.out.print("Enter HTTP Request: ");
                 String httpRequest = inputBuffer.readLine();
                 String[] requestComponents = (httpRequest.trim().split("\\s+"));
-                if (requestComponents.length != 3) {
-                    // bad request
-                    System.out.println("Bad request! Usage: java UDPClient GET TestFile.html HTTP/2.0");
-                    return;
+                if (!handleBadRequest(requestComponents)) {
+                    continue;
                 }
+                // otherwise perform transaction
                 fileName = requestComponents[1];
-                client.sendHTTPRequest(httpRequest);
-                client.loadFileFromResponse();
-                clientSocket.close();
+                client.executeTransaction(httpRequest);
             } catch (IOException e) {
                 System.out.println("Client error " + e.getLocalizedMessage());
-                exit(1);
             }
         }
     }
+
+    private static boolean handleBadRequest(String[] requestComponents) {
+        if (requestComponents.length != 3) {
+            // bad request
+            System.out.println("Bad request!\n Usage: java UDPClient GET TestFile.html HTTP/2.0");
+            return false;
+        }
+        if (!requestComponents[1].contains(".")) {
+            // bad file name
+            System.out.println("Bad request!\n Couldn't parse file name.");
+            return false;
+        }
+        return true;
+    }
 }
 
+/**
+ * A helper for the web client
+ */
 class UDPClientHelper {
     private byte[] sendData;
     private byte[] receiveData = new byte[UDPClient.PACKET_LENGTH];
     private InetAddress ipAddress;
     private byte[] file;
+    private int fileSize;
+    private String fileInfo = "";
 
-    public UDPClientHelper(String ipAddressAssigned) {
+    /**
+     * Create a new UDPClientHelper class
+     *
+     * @param ipAddressAssigned String the assigned ip address for the request
+     */
+    UDPClientHelper(String ipAddressAssigned) {
         try {
             ipAddress = InetAddress.getByName(ipAddressAssigned);
         } catch (UnknownHostException e) {
@@ -69,49 +107,139 @@ class UDPClientHelper {
         }
     }
 
-    public void sendHTTPRequest(String request) throws IOException {
+    void executeTransaction(String request) throws IOException {
+        sendHTTPRequest(request);
+        try {
+            receiveHTTPResponse();
+        } catch (SocketTimeoutException e) {
+            System.out.println("Server not responding... \r\n\r\n");
+            UDPClient.clientSocket.close();
+            return;
+        } catch (IOException e) {
+            return;
+        }
+        loadFileFromResponse();
+        listenForNullPacket();
+        System.out.println("Finished processing input... \n\n\n");
+        return;
+    }
+
+    /**
+     * Send an HTTP request to the server
+     *
+     * @param request String the HTTP request
+     * @throws IOException E if unable to send the packet
+     */
+    private void sendHTTPRequest(String request) throws IOException {
         sendData = request.getBytes();
 
         // Create the datagram packet
         DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ipAddress, 10040);
         try {
+            UDPClient.clientSocket.setSoTimeout(2000);
             UDPClient.clientSocket.send(sendPacket);
         } catch (IOException e) {
-            System.out.println("Error sending packet... " + e.getLocalizedMessage());
-            throw e;
+            System.out.println("Unable to send HTTP request " + e.getLocalizedMessage());
         }
     }
 
-    public void loadFileFromResponse() {
-        // get the first packet
-        int fileSize = 0;
-        String fileInfo;
+    /**
+     * Listen for and process an HTTP response from the server
+     */
+    private void receiveHTTPResponse() throws IOException {
+        // Receive the HTTP request response
         try {
-            byte[] packet = receivePacket();
-            int start = new String(packet).lastIndexOf(':');
-            int end = new String(packet).indexOf("\r", start);
-            fileSize = Integer.parseInt(new String(packet).substring(start + 1, end).trim());
-            fileInfo = new String(packet).substring(81);
-        } catch (IOException | NumberFormatException e) {
-            return;
-        }
-
-        while (fileInfo.length() < fileSize) {
-            try {
-                byte[] packet = receivePacket();
-                fileInfo += new String(packet).substring(81);
-                if (packet[packet.length - 2] == 0) {
-                    // found the null terminator, write and close file
-                    file = fileInfo.getBytes();
-                    writeFile();
-                }
-            } catch (IOException e) {
+            receivePacket();
+            byte[] httpResponse = receiveData;
+            int start = new String(httpResponse).lastIndexOf(':');
+            int end = new String(httpResponse).indexOf("\r", start);
+            if (start == -1 || end == -1) {
+                System.out.println("Unable to process HTTP response -- BAD HEADER");
                 return;
             }
+            fileSize = Integer.parseInt(new String(httpResponse).substring(start + 1, end).trim());
+            int responseCode = Integer.parseInt(new String(httpResponse).substring(9, 12));
+            ResponseCodes response = getResponseString(responseCode);
+            if (response != ResponseCodes.OKAY) {
+                // invalid response
+                if (response == ResponseCodes.NOT_FOUND) {
+                    throw new IOException("404 FILE NOT FOUND");
+                } else {
+                    throw new IOException(response.toString());
+                }
+            }
+
+            System.out.println("Received HTTP response \n" + new String(receiveData));
+        } catch (SocketTimeoutException to) {
+            System.out.println("Timed out waiting for server... ");
+            throw to;
+        }
+        catch (IOException | NumberFormatException | ArrayIndexOutOfBoundsException e) {
+            System.out.println("Unable to process HTTP response " + e.getLocalizedMessage());
+            throw e;
         }
 
     }
 
+    /**
+     * After receiving the response, load the file requested
+     */
+    private void loadFileFromResponse() {
+        // get the file packets wile the size of the data is less than the file size
+        while (fileInfo.length() < fileSize) try {
+            receivePacket();
+            byte[] packet = trim(receiveData); // remove null bytes
+            String header = new String(packet).substring(0, new String(packet).indexOf('&')); // just header
+            String packetString = new String(packet).substring(new String(packet).indexOf('&') + 3); // find EOH
+            int expectedChecksum = Integer.parseInt(header.substring(header.lastIndexOf('#') + 1, header.lastIndexOf('\r')));
+            if (!checksumErrorExists(expectedChecksum, packetString.getBytes())) {
+                System.out.println("Checksum error exists!");
+                continue;
+            }
+            fileInfo += packetString;
+            System.out.println("Received data in packet \n" + new String(packet));
+            packet = null;
+        } catch (IOException | ArrayIndexOutOfBoundsException e) {
+            System.out.println("Unable to load file from response " + e.getLocalizedMessage());
+            return;
+        }
+    }
+
+    /**
+     * Asks the socket ot listen for a null packet
+     */
+    private void listenForNullPacket() {
+        try {
+            receivePacket();
+        } catch (SocketTimeoutException to) {
+            return;
+        } catch (IOException e) {
+            System.out.println("Unable to receive terminating packet..." + e.getLocalizedMessage());
+            return;
+        }
+    }
+
+    /**
+     * Called upon receipt of a null packet
+     * Write the file buffer to file
+     */
+    private void receivedNullPacket() {
+        try {
+            UDPClient.clientSocket.setSoTimeout(0);
+        } catch (Exception e) {
+            return;
+        }
+        UDPClient.clientSocket.close();
+        System.out.println("Received null packet from server... writing file");
+        file = fileInfo.getBytes();
+        if (fileInfo.getBytes().length > 0) {
+            writeFile();
+        }
+    }
+
+    /**
+     * Write the data in the open file buffer to a file
+     */
     private void writeFile() {
         File localFile = new File("./" + UDPClient.fileName);
         if (!localFile.isFile()) {
@@ -125,8 +253,10 @@ class UDPClientHelper {
         }
 
         try {
+            // write file
             Files.write(localFile.toPath(), file);
             System.out.println("Wrote file successfully!");
+            file = null;
         } catch (IOException e) {
             System.out.println("Unable to write to file! Error: " + e.getLocalizedMessage());
             return;
@@ -134,31 +264,107 @@ class UDPClientHelper {
 
     }
 
-    private byte[] receivePacket() throws IOException {
+    /**
+     * Asks the client to receive a new packet from the socket
+     *
+     * @throws IOException E if unable to receive packet
+     */
+    private void receivePacket() throws IOException {
+        receiveData = new byte[UDPClient.PACKET_LENGTH]; // empty data buffer
         DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-        try {
-            UDPClient.clientSocket.receive(receivePacket);
-        } catch (IOException e) {
-            System.out.println("Error receiving packet... " + e.getLocalizedMessage());
-            throw e;
-        }
 
-        String response = new String(receivePacket.getData());
-        System.out.println("Received message from server... " + response);
-        return receiveData;
+        while (true) {
+            try {
+                UDPClient.clientSocket.receive(receivePacket);
+            } catch (SocketTimeoutException to) {
+                System.out.println("Error receiving packet... " + to.getLocalizedMessage());
+                throw to;
+            } catch (IOException e) {
+                System.out.println("Error receiving packet... " + e.getLocalizedMessage());
+                throw e;
+            }
+
+            if (receiveData[0] != 0) {
+                return;
+            } else if (receiveData[receiveData.length - 1] != 0) {
+                return;
+            } else {
+                receivedNullPacket();
+                return;
+            }
+        }
     }
 
-//    private byte gremlin(byte packet) {
-//        // First, decide whether to damage packet or not
-//
+    /**
+     * Tries to detect an error in the checksum in the header != the data received
+     *
+     * @param expectedChecksum int the checksum received in the packet header
+     * @param packet           byte[] the packet to check against
+     * @return
+     */
+    private boolean checksumErrorExists(int expectedChecksum, byte[] packet) {
+        int checksum = 0;
+        for (Byte b : packet) {
+            checksum += b.intValue();
+        }
+
+        System.out.println("Checksum comparison finished. Expected: " + expectedChecksum + " actual: " + checksum);
+
+        return checksum == expectedChecksum;
+    }
+
+    /**
+     * Decide whether to damage a packet
+     * Damage packet if necessary
+     *
+     * @param packet the packet to check
+     * @return the changed (or not) packet
+     */
+    private byte[] gremlin(byte[] packet) {
+        // First, decide whether to damage packet or not
+
 //        if (/** we shouldn't change it */) {
 //            return packet;
 //        }
-//
-//        // We decided to damage it
-//        // Let's determine how much to damage
-//        switch (UDPClient.damagedPacketProbability) {
-//            case
-//        }
-//    }
+
+        // We decided to damage it
+        // Let's determine how much to damage
+        switch (UDPClient.damagedPacketProbability) {
+
+        }
+
+        return packet;
+    }
+
+    /**
+     * Trim a byte array of any null values
+     *
+     * @param bytes byte[] the byte array to trim
+     * @return byte[] the trimmed array
+     */
+    private byte[] trim(byte[] bytes) {
+        int i = bytes.length - 1;
+        while (i >= 0 && bytes[i] == 0) {
+            --i;
+        }
+
+        return Arrays.copyOf(bytes, i + 1);
+    }
+
+    private enum ResponseCodes {
+        UNKNOWN, OKAY, NOT_FOUND
+    }
+
+    private ResponseCodes getResponseString(int response) {
+        switch (response) {
+            case 200:
+                return ResponseCodes.OKAY;
+            case 404:
+                return ResponseCodes.NOT_FOUND;
+            default:
+                break;
+        }
+
+        return ResponseCodes.UNKNOWN;
+    }
 }
---
+++ Project 1/NetworkServer/src/UDPServerHelperTest.java
@@ -0,0 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by patrickbush on 3/9/17.
+ */
+public class UDPServerHelperTest {
+
+
+}
